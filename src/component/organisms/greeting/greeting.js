fcd69329378b726a88444dd4a89b21b9dc3e570c
AliMasykur10 <alimasykur2001@gmail.com>
2023-05-21 22:37:27 +0700

    menambahkan greeting component

---
+++ src/component/organisms/greeting/greeting.js
@@ -0,0 +1,13 @@
+export default function Greeting() {
+  return (
+    <>
+      <div className="greeting py-10 px-14 bg-grey-light">
+        <h1 className=" md:text-6xl text-2xl">Welcome</h1>
+        <p className="md:text-3xl text-base mt-3">
+          Thank you for your support. Glad you are here. <br />
+          Here’s is some information to help you getting started with this Figma File.
+        </p>
+      </div>
+    </>
+  );
+}
--- src/component/template/contiainer/container.js
+++ src/component/template/contiainer/container.js
@@ -1,9 +1,11 @@
+import Greeting from "../../organisms/greeting/greeting";
 import Hero from "../../organisms/hero/Hero";
 
 export default function Container() {
   return (
     <>
       <Hero />
+      <Greeting />
     </>
   );
 }
