85e5cbeb9691aaa9529534f89ee9fe6cb08f7167
AliMasykur10 <alimasykur2001@gmail.com>
2023-05-23 21:43:34 +0700

    menyelesaikan responsive mobile

--- src/component/molecules/descriptionAssets/descriptionAssest.js
+++ src/component/molecules/descriptionAssets/descriptionAssest.js
@@ -1,7 +1,7 @@
 export default function DescriptionAssets() {
   return (
     <>
-      <div className="description-assets py-12">
+      <div className="description-assets py-12 px-2">
         <h2 className=" md:text-2xl md:pb-2 mb-6">Assets</h2>
         <p className="mb-6">
           This design is using assets from pages like Unsplash, Pexels or other assets under a free license. You may need to install fonts that aren’t available on Figma. You are free to use all assets mentioned on the licensing page for
--- src/component/organisms/framer/framer.js
+++ src/component/organisms/framer/framer.js
@@ -5,14 +5,14 @@ export default function Framer() {
     <>
       <div className="webflow w-full md:px-32 px-3 pb-28 ">
         <h1 className="md:text-4xl text-2xl mb-12 ">Get FREE Framer Template</h1>
-        <div className="description-webflow md:grid md:grid-cols-8 md:items-center bg-grey-light p-10">
-          <img src={icon} alt="" />
-          <div className="first-webflow-comp md:col-span-5">
+        <div className="description-webflow md:grid md:grid-cols-8 md:items-center bg-grey-light md:p-10 px-3 py-10">
+          <img src={icon} alt="" className="md:mb-0 mb-10" />
+          <div className="first-webflow-comp md:col-span-5 md:mb-0 mb-8">
             <h2 className=" md:text-2xl md:mb-2">FREE Framer Template</h2>
             <p>Check out the Framer Template of this design</p>
           </div>
-          <p className="underline">Live Preveiw</p>
-          <button className=" md:w-36 md:h-12 rounded bg-black-background text-grey-light ">
+          <p className="underline md:mb-0 mb-8">Live Preveiw</p>
+          <button className=" px-3 py-2 rounded bg-black-background text-grey-light ">
             <p>Get Template</p>
           </button>
         </div>
